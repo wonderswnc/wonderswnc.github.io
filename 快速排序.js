68f2c7856963284eb4dc9f7c4782ed4dc67e5f0e
shengjun.huang@mail.dealmoon.com <shengjun.huang@mail.dealmoon.com>
2017-10-23 21:22:27 +0800

    update

--- 快速排序.js
+++ 快速排序.js
@@ -1,24 +1,17 @@
 function sortL(arr) {
-  if (arr.length === 1) {
-    return [arr[0]]
+  if (arr.length <= 0) {
+    return arr;
   }
-  if (arr.length === 0) {
-    return [];
-  }
-  var index = Math.floor(Math.random() * arr.length);
+  var index = (Math.random() * arr.length | 0);
   var currentValue = arr[index];
   var left = [], right = [];
   arr.splice(index,1);
   for (let i = 0;i < arr.length;i ++) {
-    if (arr[i] <= currentValue) {
-      left.push(arr[i])
-    } else {
-      right.push(arr[i])
-    }
+    arr[i] <= currentValue ? left.push(arr[i]) : right.push(arr[i]);
   }
   return sortL(left).concat([currentValue], sortL(right));
 }
-sortL([4,3,2,1,1,3])
+
 function range(len = 10) {
   let i = 0,arr = [];
   while(i < len) {
@@ -26,3 +19,5 @@ function range(len = 10) {
   }
   return arr;
 }
+
+sortL(range().map(item => (item * Math.random() * 10 | 0)))
