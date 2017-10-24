951fbb58faba48ee5545c906ba03827a7efd4fad
shengjun.huang@mail.dealmoon.com <shengjun.huang@mail.dealmoon.com>
2017-10-24 10:14:34 +0800

    update

---
+++ js.js
@@ -0,0 +1,13 @@
+function closure() {
+  for (let i = 0; i < 10; i ++) {
+    setTimeout(function() {
+      consoleL(i);
+    },0);
+    console.log(i);
+  }
+  console.log(`************************`);
+}
+closure();
+function consoleL(i) {
+  console.log(i)
+}
